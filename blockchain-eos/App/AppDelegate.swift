2fa6a830cc01df1b4932d8499438c1bf645a7e4f
Tai Ho <tai.ho@snapsheet.me>
2020-03-22 16:03:12 -0500

    cleanup

--- blockchain-eos/App/AppDelegate.swift
+++ blockchain-eos/App/AppDelegate.swift
@@ -21,9 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             self.stubNetworking()
         }
         
-        UILabel.appearance().textColor = Constants.UI.AppTextColor
-        UIButton.appearance().setTitleColor( .white, for: .normal)
-        UITextView.appearance().textColor =  Constants.UI.AppTextColor
+        setAppColors()
         
         return true
     }
@@ -40,6 +38,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    private func setAppColors() {
+        UILabel.appearance().textColor = Constants.UI.AppTextColor
+        UIButton.appearance().setTitleColor( .white, for: .normal)
+        UITextView.appearance().textColor =  Constants.UI.AppTextColor
     }
 
     #if DEBUG
