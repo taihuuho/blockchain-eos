7536d308baefd1439b55da097b547f8bd47b0e0c
Tai Ho <tai.ho@snapsheet.me>
2020-03-21 22:05:04 -0500

    Networking tests

--- blockchain-eosTests/Functional Tests/NetworkingTests.swift
+++ blockchain-eosTests/Functional Tests/NetworkingTests.swift
@@ -10,25 +10,118 @@ import XCTest
 
 @testable import blockchain_eos
 
-class NetworkingTests: XCTest {
+final class MockURLSession: URLSession {
+    private var dataTask: MockURLSessionDataTask!
+    
+    override init() {}
+    
+    func setMockResponse(data: Data? = nil, response: URLResponse? = nil, error: Error? = nil) {
+        dataTask = MockURLSessionDataTask()
+        dataTask.taskResponse = (data, response, error)
+    }
+    override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        let (data, response, error) = self.dataTask.taskResponse!
+        if let response = response as? HTTPURLResponse {
+            let newResponse = HTTPURLResponse(url: request.url!, statusCode: response.statusCode, httpVersion: "1.1", headerFields: response.allHeaderFields as? [String : String])
+            dataTask.taskResponse = (data, newResponse, error)
+        }
+        dataTask.completionHandler = completionHandler
+        return dataTask
+    }
+}
+
+final class MockURLSessionDataTask: URLSessionDataTask {
+    typealias CompletionHandler = (Data?, URLResponse?, Error?) -> Void
+    var completionHandler: CompletionHandler?
+    var taskResponse: (Data?, URLResponse?, Error?)?
+    
+    override init() {}
+    
+    override func resume() {
+      DispatchQueue.main.async {
+        self.completionHandler?(
+            self.taskResponse?.0,
+            self.taskResponse?.1,
+            self.taskResponse?.2
+        )
+      }
+    }
+}
+
+class NetworkingTests: XCTestCase {
+    let mockServerEndpoint = "http://localhost"
+    var apiClient: EosApi!
+    var mockUrlSession: MockURLSession!
+    var baseURL: URL!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        mockUrlSession = MockURLSession()
+        baseURL = URL(string: mockServerEndpoint)!
+        apiClient = EosApiImpl(endpoint: mockServerEndpoint, urlSession: mockUrlSession)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mockUrlSession = nil
+        apiClient = nil
     }
 
-    func testExample() {
+    func testGetInfoSuccess() {
+        let getInfoData = MockDataProvider.getInfoJSON.data(using: .utf8)
+        let jsonDecoder = JSONDecoder()
+        let mockEosInfo = try! jsonDecoder.decode(EosInfo.self, from: getInfoData!)
+        let urlResponse = HTTPURLResponse(url: baseURL, statusCode: 200, httpVersion: "1.0", headerFields: nil)
+        mockUrlSession.setMockResponse(data: getInfoData, response: urlResponse, error: nil)
+        apiClient.getChainInfo { (result) in
+            switch result {
+            case .success(let eosInfo):
+                XCTAssertTrue(eosInfo.headBlockId == mockEosInfo.headBlockId)
+                break
+            case .failure(_):
+                XCTAssertFalse(true)
+                break
+            }
+        }
+    }
+    
+    func testGetBlockSuccess() {
+        let getBlockData = MockDataProvider.getEosBlockJSON.data(using: .utf8)
+        let jsonDecoder = JSONDecoder()
+        let mockEosBlock = try! jsonDecoder.decode(EosBlock.self, from: getBlockData!)
+        let urlResponse = HTTPURLResponse(url: baseURL, statusCode: 200, httpVersion: "1.0", headerFields: nil)
+        mockUrlSession.setMockResponse(data: getBlockData, response: urlResponse, error: nil)
         
+        apiClient.getBlock(blockId: mockEosBlock.id) { (result) in
+            switch result {
+            case .success(let eosBlock):
+                XCTAssertTrue(eosBlock.id == mockEosBlock.id)
+                break
+            case .failure(_):
+                break
+            }
+        }
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
+    
+    func testGetBlockFailure() {
+        
+        let jsonDecoder = JSONDecoder()
+        let getBlockData = MockDataProvider.getEosBlockJSON.data(using: .utf8)!
+        let mockEosBlock = try! jsonDecoder.decode(EosBlock.self, from: getBlockData)
+        
+        let apiErrorData = MockDataProvider.getEosBlockJSONError.data(using: .utf8)!
+        let mockApiError = try! jsonDecoder.decode(ApiError.self, from: apiErrorData)
+        
+        let urlResponse = HTTPURLResponse(url: baseURL, statusCode: 500, httpVersion: "1.0", headerFields: nil)
+        mockUrlSession.setMockResponse(data: apiErrorData, response: urlResponse, error: nil)
+        
+        apiClient.getBlock(blockId: mockEosBlock.id) { (result) in
+            switch result {
+            case .success(_):
+                break
+            case .failure(let error):
+                XCTAssertTrue(error.code == mockApiError.code)
+                XCTAssertTrue(error.message == mockApiError.message)
+                break
+            }
+        }
     }
-
 }
