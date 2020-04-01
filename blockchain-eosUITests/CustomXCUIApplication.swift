9fa3705e834a1dda25c88979821e75ffaf231a01
Tai Ho <12061272+taihuuho@users.noreply.github.com>
2020-03-31 22:30:13 -0500

    Use Swifter to create Mock server instead of mocking with OHHTPStubs (#25)
    * Pick up Swifter, removed OHTTPStubs
    
    * Use mock server instead of mocking data in app bundle

--- blockchain-eosUITests/CustomXCUIApplication.swift
+++ blockchain-eosUITests/CustomXCUIApplication.swift
@@ -7,14 +7,39 @@
 //
 
 import XCTest
+import Swifter
 
 class CustomXCUIApplication: XCUIApplication {
+    
+    static let mockServer: HttpServer = createMockServer()
+    
     override func launch() {
-        self.launchArguments = [ "--uitesting" ]
+        if !CustomXCUIApplication.mockServer.operating{
+            try! CustomXCUIApplication.mockServer.start(80)
+        }
         self.launchEnvironment = [
-            "GET_INFO_SUCCESS" : TestContants.getInfoJSONSuccess,
-            "GET_BLOCK_SUCCESS" : TestContants.getEosBlockJSON
+            "MOCK_SERVER" : "http://127.0.0.1"
         ]
         super.launch()
+    }
+    
+    private static func createMockServer() -> HttpServer {
+
+        let server = HttpServer()
+        
+        let getInfoData = TestContants.getInfoJSONSuccess.data(using: .utf8)!
+        let getInfoResponse = try! JSONSerialization.jsonObject(with: getInfoData, options: [.allowFragments])
+        
+        server.POST["/v1/chain/get_info"] = { request in
+            return HttpResponse.ok(.json(getInfoResponse))
+        }
+        
+        let getBlockData = TestContants.getEosBlockJSON.data(using: .utf8)!
+        let getBlockResponse = try! JSONSerialization.jsonObject(with: getBlockData, options: [.allowFragments])
+        server.POST["/v1/chain/get_block"] = { request in
+            return HttpResponse.ok(.json(getBlockResponse))
+        }
+        
+        return server
     }
 }
